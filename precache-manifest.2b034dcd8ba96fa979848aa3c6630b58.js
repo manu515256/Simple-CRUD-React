a3e663b912890d7d618b88d0a4ccf14175ec35cd
Manu515 <manudesarollo@gmail.com>
2020-05-22 13:30:54 -0300

    Updates

---
+++ precache-manifest.2b034dcd8ba96fa979848aa3c6630b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad6da0a4a1b4335b13af698b058e200",
+    "url": "/Simple-CRUD-React/index.html"
+  },
+  {
+    "revision": "ad6e0ad66786b0320426",
+    "url": "/Simple-CRUD-React/static/css/main.0685e9b5.chunk.css"
+  },
+  {
+    "revision": "d328b1224bb08daf4eba",
+    "url": "/Simple-CRUD-React/static/js/2.6dc20f9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-CRUD-React/static/js/2.6dc20f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6e0ad66786b0320426",
+    "url": "/Simple-CRUD-React/static/js/main.cfc63c4d.chunk.js"
+  },
+  {
+    "revision": "49cee8702e41e4cd0e98",
+    "url": "/Simple-CRUD-React/static/js/runtime-main.116cd073.js"
+  }
+]);
